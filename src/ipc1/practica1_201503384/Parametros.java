b50ea576be05e3e6a331cc92e910debd54d29f03
diego <diego@192.168.0.21>
2018-02-22 23:26:43 -0600

    Practica1 en proceso

---
+++ src/ipc1/practica1_201503384/Parametros.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica1_201503384;
+
+/**
+ *
+ * @author diego
+ */
+public class Parametros {
+    private int jugadores, subidas, bajones;
+
+    /**
+     * @return the jugadores
+     */
+    public int getJugadores() {
+        return jugadores;
+    }
+
+    /**
+     * @param jugadores the jugadores to set
+     */
+    public void setJugadores(int jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    /**
+     * @return the subidas
+     */
+    public int getSubidas() {
+        return subidas;
+    }
+
+    /**
+     * @param subidas the subidas to set
+     */
+    public void setSubidas(int subidas) {
+        this.subidas = subidas;
+    }
+
+    /**
+     * @return the bajones
+     */
+    public int getBajones() {
+        return bajones;
+    }
+
+    /**
+     * @param bajones the bajones to set
+     */
+    public void setBajones(int bajones) {
+        this.bajones = bajones;
+    }
+}
